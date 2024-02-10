57e60be46ff98a03f0576d648f195bc2ad9043dc
Shyam1806 <62296718+Shyam1806@users.noreply.github.com>
2024-02-10 20:43:33 +0530

    "Some ui modification"

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+     <div className="">
+        <p>Design and Developed  by Shyam</p>
+     </div>
+    </>
+  )
+}
+
+export default Footer;
